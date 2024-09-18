30a1abd52fd347752c89b5b7d195476a7521d9fb
Jere Jyrinki <jyrinki94@gmail.com>
2024-09-18 21:47:24 +0300

    valmis

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Appbar, Provider as PaperProvider } from 'react-native-paper';
+
+
+const CustomAppBar = ({ navigation, back }) => {
+  return (
+    <Appbar.Header>
+      {back ? (
+        <Appbar.BackAction onPress={navigation.goBack} />
+      ) : (
+        <Appbar.Action
+          icon="arrow-right"
+          onPress={() => navigation.navigate('Second')}
+        />
+      )}
+      <Appbar.Content title="My App" />
+    </Appbar.Header>
+  );
+};
+
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button title="Go to Second Screen" onPress={() => navigation.navigate('Second')} />
+    </View>
+  );
+};
+
+
+const SecondScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Second Screen</Text>
+    </View>
+  );
+};
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={({ navigation, route }) => ({
+            header: (props) => <CustomAppBar {...props} />,
+          })}
+        >
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Second" component={SecondScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
